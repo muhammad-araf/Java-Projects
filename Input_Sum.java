e701cb01c3daead0608de82f9593ed5f7d249a8d
m-araf <lentoaraf@gmail.com>
2025-02-22 17:34:00 +0500

    Input 2 Number in 2 variable and sum and print

---
+++ Input_Sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Input_Sum {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int var1 = scan.nextInt();
+        int var2 = scan.nextInt();
+        int var3 = var1 + var2;
+        System.out.println(var3);
+    }
+}
