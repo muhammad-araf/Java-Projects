423890a9fa624efd32e38a59f81099ba8fe50660
m-araf <lentoaraf@gmail.com>
2025-02-22 16:36:55 +0500

    Input in variable

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Input {
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String name = scan.next();
+            System.out.println("Your Name : "+name);
+
+
+    }
+}
