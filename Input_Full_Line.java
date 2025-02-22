b48661b77798b71f6b5802351c3e1b5d9274acd9
m-araf <lentoaraf@gmail.com>
2025-02-22 17:17:32 +0500

    Input Complete Line in String

---
+++ Input_Full_Line.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Input_Full_Line {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println("Name : "+name);
+    }
+}
