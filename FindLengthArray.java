8fc3ea1136ca7e560c384bc9bf89fdcd892f3d96
m-araf <lentoaraf@gmail.com>
2025-02-22 22:27:56 +0500

    Declare , input and print array

---
+++ FindLengthArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FindLengthArray{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i = 0;i<5;i++){
+            arr[i] = scan.nextInt();
+        }
+        for(int i = 0;i<5;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+}
