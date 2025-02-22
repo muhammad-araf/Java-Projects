05b9ea3c5370d82f6651d2a3ed0453e99611d2a2
m-araf <lentoaraf@gmail.com>
2025-02-22 20:02:48 +0500

    Take input 2 number and Sum in UDF

---
+++ UDF.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class UDF {
+    public static int sum(int a , int b){
+        return a + b;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int sum = sum(a,b);
+        System.out.print(sum);
+    }
+}
