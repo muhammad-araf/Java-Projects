ec7ebe580e82a86ac4f5ae4b57f46f610e83c660
m-araf <lentoaraf@gmail.com>
2025-02-23 11:33:05 +0500

    In String '2'+'2' = '22'

---
+++ StringNumAdd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringNumAdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s1 = "10";
+        String s2 = "10";
+        String s3 = s1+s2;
+        System.out.println(s3); //Output: 1010
+    }
+}
