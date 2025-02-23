8c846e27fc48b2c9cfab6b24f3328a13bfffe651
m-araf <lentoaraf@gmail.com>
2025-02-23 12:17:59 +0500

    Declare and initialize String Array and print

---
+++ StringArray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class StringArray {
+    public static void main(String[] args){
+        String[] name = {"Araf", "lento","Fara"};
+        for(int i = 0;i<3;i++){
+            System.out.println(name[i]);
+        }
+    }
+}
