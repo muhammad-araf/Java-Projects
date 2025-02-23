3d7a457121abbeb213b7be9c6a8941a203cc7b0e
m-araf <lentoaraf@gmail.com>
2025-02-23 19:05:37 +0500

    Declare and call non-static Method using Object

---
+++ Methods/MethodObject.java
@@ -0,0 +1,9 @@
+public class MethodObject{
+    void hello(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args){
+        MethodObject araf = new MethodObject();
+        araf.hello();
+    }
+}
