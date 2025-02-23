c17e73ffc5b99dfe47aba4dcbd4f31d7bedc2c18
m-araf <lentoaraf@gmail.com>
2025-02-23 11:44:30 +0500

    Find Max number of 2 variable Using Built In function Math.max

---
+++ FindMaxWithBuiltin.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FindMaxWithBuiltin{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = 5;
+        int b = 7;
+        System.out.println("The Greater Number is "+Math.max(a,b));
+
+    }
+}
