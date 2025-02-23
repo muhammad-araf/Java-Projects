2be8be8a81cca22462da43b7752b978796a2b11a
m-araf <lentoaraf@gmail.com>
2025-02-23 12:35:55 +0500

    Use For each Loop to print Array element

---
+++ ForEach.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ForEach {
+    public static void main(String[] args){
+        int[] number = {1,2,3,4};
+        for(int i : number){
+            System.out.println(i);
+        }
+    }
+}
