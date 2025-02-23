e4d0d4850ddd0d26b1c7ff5bd4ebde7f6bdd00dd
m-araf <lentoaraf@gmail.com>
2025-02-23 18:12:52 +0500

    Declare and call method

---
+++ Methods/CreateMethod.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CreateMethod {
+    static int araf(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        System.out.println(araf(a,b));
+    }
+}
