8aed4cf045d1e801d091928a81c864b1b6fd998b
m-araf <lentoaraf@gmail.com>
2025-02-23 11:56:16 +0500

    Guessing Nunber with random number generator

---
+++ RandomNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class RandomNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        do{
+        int randnum = (int) (Math.random() * 5);
+        System.out.println("Enter a Guess number");
+        int guess = scan.nextInt();
+        if(randnum == guess){
+            System.out.println("U guessed right ");
+        }else{
+            System.out.println("U guessed wrong the correcr is "+ randnum);  
+        }
+    }while(true);
+    }
+}
