7e1df29858e19af21650445c23dacd5730d6f6f6
m-araf <lentoaraf@gmail.com>
2025-02-28 06:07:27 +0500

    Static Method

---
+++ Methods/StaticMethod.java
@@ -0,0 +1,8 @@
+public class StaticMethod {
+    static void print() {
+        System.out.println("Hello, World!\n");
+    }
+            public static void main(String[] args){
+        print();
+    }
+}
