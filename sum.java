640c45591afde57315e038e26ad7144ab1ff1b6a
m-araf <lentoaraf@gmail.com>
2025-03-06 16:18:47 +0500

    Sum 2 number

---
+++ sum.java
@@ -0,0 +1,10 @@
+class hello{
+    int a = 5;
+    int b = 5;
+}
+public class sum{
+    public static void main(String[] args){
+        hello h = new hello();
+        System.out.println(h.a+h.b);
+    }
+}
