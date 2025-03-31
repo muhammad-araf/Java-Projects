271a16b09ee1474da6d3bf288bb18341949d3b27
m-araf <lentoaraf@gmail.com>
2025-03-31 16:34:10 +0500

    Correct the code

--- DigitalRoot.java
+++ DigitalRoot.java
@@ -1,5 +1,5 @@
 public class DigitalRoot {
-    public static int digitalRoot(int num) {
+    public static int root(int num) {
         while (num >= 10) {
             int sum = 0;
             while (num > 0) {
@@ -13,6 +13,6 @@ public class DigitalRoot {
 
     public static void main(String[] args) {
         int number = 987;
-        System.out.println("Digital Root of " + number + " is: " + digitalRoot(number));
+        System.out.println("Digital Root of " + number + " is: " + root(number));
     }
 }
