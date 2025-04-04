62dbbc7189009ad4fbef19ddf501b0c45e37a0a3
m-araf <lentoaraf@gmail.com>
2025-04-05 03:11:20 +0500

    Remove Temp File

--- ObjectOrientedProgramming/tempCodeRunnerFile.java
+++ ObjectOrientedProgramming/tempCodeRunnerFile.java
@@ -1,18 +1,1 @@
-class student {
-    String name;
-    int age;
 
-    void print() {
-        System.out.println("Your Name : " + name);
-        System.out.println("Your Age : " + age);
-    }
-}
-public class ClassObject {
-    
-    public static void main(String[] args){
-        student s1 = new student();
-        s1.name = "Araf";
-        s1.age = 17;
-        s1.print();
-    }
-}   
