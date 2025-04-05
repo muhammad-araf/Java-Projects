683dd6bfd4ff9d8b7ca994387b98dcc336b6614b
m-araf <lentoaraf@gmail.com>
2025-04-06 03:22:00 +0500

    Input All Data Type in java

--- Input.java
+++ Input.java
@@ -1,10 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            String name = scan.next();
-            System.out.println("Your Name : "+name);
-
-
+           Scanner scan = new Scanner(System.in);
+    int n;
+    n = nextInt();
+        String name;
+        name = nextLine();
+            double marks;
+            marks = nextDouble();
+              float marks1;
+                marks1 = nextFloat();
+                    char ch;
+                    ch = nextChar();
+                        boolean b;
+                        b = nextBoolean();
+                            byte b1;
+                            b1 = nextByte();
+                                short s;
+                                s = nextShort();
+                                    long l;
+                                    l = nextLong();
     }
 }
