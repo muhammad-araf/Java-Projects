722f1e8757b49dc3f39af220d0efd1e8e3219dd9
m-araf <lentoaraf@gmail.com>
2025-04-08 00:31:18 +0500

    Constructor of class

---
+++ Preparation/Constructor.java
@@ -0,0 +1,11 @@
+class Cons{
+    private String name;
+    Cons(String name2){
+        name = name2;
+    }
+}
+public class constructor {
+    public static void main(String[] args){
+        Cons c = new Cons("Araf");
+    }
+}
