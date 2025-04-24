58a59d7a7a650e8b78a7bb38a3a179711e5d6d34
m-araf <lentoaraf@gmail.com>
2025-04-24 18:17:46 +0500

    For Each in java

--- ForEach.java
+++ ForEach.java
@@ -3,6 +3,7 @@ public class ForEach {
     public static void main(String[] args){
         int[] number = {1,2,3,4};
         for(int i : number){
+            
             System.out.println(i);
         }
     }
